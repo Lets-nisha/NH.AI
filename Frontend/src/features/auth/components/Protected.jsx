007837c1a4f5d358e59fdad533b2a37a6cb1238e
Lets-nisha <nishaturkey7500@gmail.com>
2026-08-02 23:48:17 +0530

    Fix embedded Frontend repository

---
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import { Navigate } from "react-router"
+
+import React from 'react'
+
+const Protected = ({ children }) => {
+    const { loading, user } = useAuth()
+
+    if (loading) {
+        return (<main className="loader-container">
+            <div className="loader"></div>
+        </main>)
+    }
+    if (!user) {
+        return <Navigate to={"/login"} />
+    }
+    return children
+
+}
+
+export default Protected    
